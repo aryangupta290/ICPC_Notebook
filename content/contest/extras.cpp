1678bcedd8bd532e11dfc7c07a020263f47eec55
anurudhp <anurudhp97@gmail.com>
2021-09-13 07:13:47 +0530

    cleanup extras.cpp

--- content/contest/extras.cpp
+++ content/contest/extras.cpp
@@ -9,24 +9,27 @@ void __f(const char* names, Arg1&& arg1, Args&&... args) {
   __f(comma+1, args...);
 }
 
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
-using namespace __gnu_pbds; // OST: find_by_order, order_of_key
-typedef tree<int, null_type, less<>, rb_tree_tag,
-  tree_order_statistics_node_update> ordered_set;
 // #pragma GCC optimize("Ofast")
-// optimize("unroll-loops")
-// target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+// #pragma GCC optimize("unroll-loops")
+// target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx")
+
 /*** RNGS ***/
-SEED=chrono::steady_clock::now().time_since_epoch().count();
-// or high_resolution_clock
-mt19937 rng(SEED); /*or*/ random_device rd; mt19937 rng(rd());
-uniform_int_distribution<> dis(MIN, MAX);// usage: dis(rng)
-struct chash {int operator()(int x) const {return x^SEED;}};
-gp_hash_table<int,int,chash>m;//use cc if very less updates
+SEED = chrono::steady_clock::now().time_since_epoch().count();
+// or `high_resolution_clock`
+mt19937 rng(SEED); /* or */ random_device rd; mt19937 rng(rd());
+uniform_int_distribution<> dis(MIN, MAX); // usage: dis(rng)
+
 /*** FAST IO ***/
-inline int scan(){bool y=0; int x=0; char c=getchar_unlocked();
-while(c<'0'||c>'9'){ if(c=='-')y=1; c=getchar_unlocked();} 
-while(c>='0'&&c<='9'){x=(x<<1)+(x<<3)+c-'0';
-c=getchar_unlocked();}
-return y?-x:x; }
+inline int scan() {
+  bool y = 0; int x = 0;
+  char c = getchar_unlocked();
+  while (c < '0' || c > '9') {
+    if (c == '-') y = 1;
+    c = getchar_unlocked();
+  }
+  while (c >= '0' && c <= '9') {
+    x = (x << 1) + (x << 3) + c - '0';
+    c = getchar_unlocked();
+  }
+  return y ? -x : x;
+}
