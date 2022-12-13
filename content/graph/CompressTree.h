87db06d62591152e8439a0973797316e43ac1441
keyur <keyur@pop-os.localdomain>
2022-12-13 13:58:03 +0530

    Auxilliary Tree

--- content/graph/CompressTree.h
+++ content/graph/CompressTree.h
@@ -11,27 +11,16 @@
  * Status: Tested at CodeForces
  */
 #pragma once
-
-#include "LCA.h"
-
-typedef vector<pair<int, int>> vpi;
-vpi compressTree(LCA& lca, const VI& subset) {
-	static VI rev; rev.resize(SZ(lca.time));
-	VI li = subset, &T = lca.time;
-	auto cmp = [&](int a, int b) { return T[a] < T[b]; };
-	sort(ALL(li), cmp);
-	int m = SZ(li)-1;
-	REP(i,0,m) {
-		int a = li[i], b = li[i+1];
-		li.push_back(lca.lca(a, b));
-	}
-	sort(ALL(li), cmp);
-	li.erase(unique(ALL(li)), li.end());
-	REP(i,0,SZ(li)) rev[li[i]] = i;
-	vpi ret = {PII(0, li[0])};
-	REP(i,0,SZ(li)-1) {
-		int a = li[i], b = li[i+1];
-		ret.emplace_back(rev[lca.lca(a, b)], b);
-	}
-	return ret;
-}
+bool cmp(int u,int v){return arr[u]<arr[v];}
+int create_tree(){//return root of tree
+set<int> S;//get distinct nodes
+REP(i,k)S.insert(Q[i]);k=0;for(auto it : S)Q[k++]=it;
+sort(Q,Q+k,cmp);int kk = k;//distinct initial nodes
+//add lca of adjacent pairs
+for(int i=0;i<kk-1;i++){int x = lca(Q[i],Q[i+1]);
+if(S.count(x))continue;Q[k++]=x;S.insert(x);
+}sort(Q,Q+k,cmp);stack<int> s;s.push(Q[0]);
+for(int i=1;i<k;i++){
+while(!anc(s.top(),Q[i]))s.pop();
+tree[s.top()].PB(Q[i]);tree[Q[i]].PB(s.top());
+s.push(Q[i]);}return Q[0];}
