e108dfdb386fd609e60c2b7905281b84495e24c9
Aryan Gupta <aryan.gupta@students.iiit.ac.in>
2022-12-21 18:02:57 +0530

    final 11pt

--- content/data-structures/HashMap.h
+++ content/data-structures/HashMap.h
@@ -11,32 +11,19 @@
 
 #include <bits/extc++.h> /** keep-include */
 // To use most bits rather than just the lowest ones:
-struct chash {
-    const uint64_t RANDOM = (uint64_t)(make_unique<char>().get()) ^ chrono::high_resolution_clock::now().time_since_epoch().count();
-    static unsigned hash32(unsigned x) {  // For 4 byte datatypes only
-        x = (x ^ (x >> 16)) * 0x45d9f3b;
-        x = (x ^ (x >> 16)) * 0x45d9f3b;
-        return x ^ (x >> 16);
-    }
-    static uint64_t splitmix64(uint64_t x) {  // For 8 byte datatypes
-        // http://xorshift.di.unimi.it/splitmix64.c
-        x += 0x9e3779b97f4a7c15;
-        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
-        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
-        return x ^ (x >> 31);
-    }
-    template <typename T>
-    uint64_t operator()(T x) const {
-        return sizeof(x) <= 4 ? hash32(unsigned(x ^ RANDOM)) : (splitmix64(x) ^ RANDOM);
-    }
+#include <bits/extc++.h> /** keep-include */
+// To use most bits rather than just the lowest ones:
+struct chash { // large odd number for C
+	const uint64_t C = ll(4e18 * acos(0)) | 71;
+	ll operator()(ll x) const { return __builtin_bswap64(x*C); }
 };
-using hash_table = gp_hash_table<int, int, chash>;
-/** For CodeForces, or other places where hacking might be a problem:
+__gnu_pbds::gp_hash_table<ll,int,chash> h({},{},{},{},{1<<16});
 
+/** For CodeForces, or other places where hacking might be a problem:
 const int RANDOM = chrono::high_resolution_clock::now().time_since_epoch().count();
 struct chash { // To use most bits rather than just the lowest ones:
-        const uint64_t C = ll(4e18 * acos(0)) | 71; // large odd number
-        ll operator()(ll x) const { return __builtin_bswap64((x^RANDOM)*C); }
+	const uint64_t C = ll(4e18 * acos(0)) | 71; // large odd number
+	ll operator()(ll x) const { return __builtin_bswap64((x^RANDOM)*C); }
 };
 __gnu_pbds::gp_hash_table<ll, int, chash> h({},{},{},{}, {1 << 16});
 */
---
+++ content/number-theory/primRoot.h
@@ -0,0 +1,26 @@
+/**
+ * Author: Simon Lindholm
+ * Date: 2015-06-23
+ * License: CC0
+ * Source: own work
+ * Description: Calculates ncr for large N and prime Mod
+ * Status: Tested for all |k|,|c|,to,m <= 50, and on kattis:aladin
+ */
+#pragma once
+int PrimRoot(int p, int x) {
+    // finds primitive root of prime p greater than x(If it doesnt exist, returns 0)
+    vector<int> v;int t = p - 1;
+    REP(i, 2, t + 1) {
+        if (i * i > t) break;
+        if (t % i == 0) {
+            v.PB((p - 1) / i);
+            while (t % i == 0) {
+                t /= i;}}}
+    if (t > 1) v.PB((p - 1) / t);
+    REP(i, x + 1, p) {
+        ll flag = 0;
+        REP(j, 0, ((ll)v.size())) {
+            if (powM(i, v[j], p) == 1) {
+                flag = 1;break;}}
+        if (flag == 0) {
+            return i;}}return 0;}
